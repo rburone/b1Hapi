0918c549c1b724b74fe8004befdf4899cda6f8f2
Ricardo Burone <rburone@users.noreply.github.com>
2024-08-30 10:46:59 -0300

    Add config to model

--- customdata/models.js
+++ customdata/models.js
@@ -87,6 +87,7 @@ module.exports = [
         dataSource: 'mongodb4',
         actions   : [
             { name: 'search', permissions: [] },
+            { name: 'findID', permissions: [] },
         ]
     },
     { // Activos_count
